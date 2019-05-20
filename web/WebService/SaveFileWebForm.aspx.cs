0ffc7cd1fbe749ec42dcf95b7777535f42ea7209
halfmoon2014 <mrzhangmh@gmail.com>
2019-05-20 10:29:26 +0800

    upload

--- web/WebService/SaveFileWebForm.aspx.cs
+++ web/WebService/SaveFileWebForm.aspx.cs
@@ -1,36 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
- 
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
- 
-public partial class tl_yf_Default : System.Web.UI.Page
+using System.Text;
+public partial class WebService_SaveFileWebForm : System.Web.UI.Page
 {
-
     protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Files.Count > 0)
         {
-            if (Request.Files.Count > 0)
+            try
             {
-                try
-                {
-                    //得到客户端上传的文件
-                    HttpPostedFile file = Request.Files[0];
-                    //服务器端要保存的路径
-                    string filePath = "G:\\TEST\\" + file.FileName + ".rtf";
-                    file.SaveAs(filePath);
-                    //返回结果
-                    Response.Write("Success");
-                }
-                catch
-                {
-                    Response.Write("Error");
-                }
-            }
-            else
-            {
-                Response.Write("Error1");
-            } 
-        }
+                //得到客户端上传的文件
+                HttpPostedFile file = Request.Files[0];
+                Encoding gb = Encoding.GetEncoding("utf-8");
+                byte[] bytes = gb.GetBytes(file.FileName);
 
+                //服务器端要保存的路径
+                string filePath = "d:\\web_tlerp\\MyUpload\\file\\" + System.Web.HttpUtility.UrlDecode(file.FileName, System.Text.Encoding.UTF8) + ".rtf";
+                file.SaveAs(filePath);
+                //返回结果
+                Response.Write("Success");
+            }
+            catch
+            {
+                Response.Write("Error");
+            }
+        }
+        else
+        {
+            Response.Write("Error1");
+        }
+    }
 }
